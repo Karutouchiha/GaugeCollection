4e9d97576817cd05acb07623004195f842b5195c
Asar Lara <lara.asar@htl-wels.at>
2020-11-04 13:39:15 +0100

    init commit

--- src/GaugeBase.java
+++ src/GaugeBase.java
@@ -1,14 +1,13 @@
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-
 
 public class GaugeBase extends Group {
 
-    private int width = 100;
-    private int height = 100;
+    private int width = 50;
+    private int height = 50;
     private int value = 0;
 
     public GaugeBase()
@@ -18,31 +17,150 @@ public class GaugeBase extends Group {
 
     public void redraw()
     {
+        this.getChildren().clear();
+
         Rectangle border = new Rectangle();
         border.setX(0);
         border.setY(0);
         border.setWidth(width);
         border.setHeight(height);
-        border.setArcWidth(20);
-        border.setArcHeight(20);
-        border.setStrokeWidth(10);
-        border.setStroke(Color.RED);
-        border.setFill(Color.GRAY);
+        border.setStrokeWidth(5);
+        border.setStroke(Color.BLACK);
+        border.setFill(Color.TRANSPARENT);
 
-        Text t = new Text();
-        t.setText(String.valueOf(this.value));
-        t.setFont(Font.font ("Verdana", 20));
-        t.setFill(Color.BLACK);
+        Rectangle blue = new Rectangle();
+        blue.setWidth(20);
+        blue.setHeight(70);
+        blue.setX(5);
+        blue.setY(5);
+        blue.setFill(Color.LIGHTSKYBLUE);
 
-        //measure the text
-        double w = t.getLayoutBounds().getWidth();
-        double h = t.getLayoutBounds().getHeight();
+        Rectangle bluegreen = new Rectangle();
+        bluegreen.setWidth(20);
+        bluegreen.setHeight(70);
+        bluegreen.setX(30);
+        bluegreen.setY(5);
+        bluegreen.setFill(Color.MEDIUMSPRINGGREEN);
 
-        //calculate center
-        t.setX(this.width / 2.0 - w / 2.0);
-        t.setY(this.height / 2 - h / 2.0);
+        Rectangle green = new Rectangle();
+        green.setWidth(20);
+        green.setHeight(70);
+        green.setX(55);
+        green.setY(5);
+        green.setFill(Color.CHARTREUSE);
 
-        this.getChildren().addAll(border, t);
+        Rectangle greenyellow = new Rectangle();
+        greenyellow.setWidth(20);
+        greenyellow.setHeight(70);
+        greenyellow.setX(80);
+        greenyellow.setY(5);
+        greenyellow.setFill(Color.GREENYELLOW);
+
+        Rectangle yellow = new Rectangle();
+        yellow.setWidth(20);
+        yellow.setHeight(70);
+        yellow.setX(105);
+        yellow.setY(5);
+        yellow.setFill(Color.YELLOW);
+
+        Rectangle yelloworange = new Rectangle();
+        yelloworange.setWidth(20);
+        yelloworange.setHeight(70);
+        yelloworange.setX(130);
+        yelloworange.setY(5);
+        yelloworange.setFill(Color.GOLD);
+
+        Rectangle orange = new Rectangle();
+        orange.setWidth(20);
+        orange.setHeight(70);
+        orange.setX(155);
+        orange.setY(5);
+        orange.setFill(Color.DARKORANGE);
+
+        Rectangle orangered = new Rectangle();
+        orangered.setWidth(20);
+        orangered.setHeight(70);
+        orangered.setX(180);
+        orangered.setY(5);
+        orangered.setFill(Color.ORANGERED);
+
+        Rectangle red = new Rectangle();
+        red.setWidth(20);
+        red.setHeight(70);
+        red.setX(205);
+        red.setY(5);
+        red.setFill(Color.RED);
+
+        Rectangle redpurple = new Rectangle();
+        redpurple.setWidth(20);
+        redpurple.setHeight(70);
+        redpurple.setX(230);
+        redpurple.setY(5);
+        redpurple.setFill(Color.FIREBRICK);
+
+        Text txtE = new Text("Set a Value between 0 and 100! \n       To charge the battery!");
+        txtE.setLayoutX(40);
+        txtE.setLayoutY(100);
+        this.getChildren().add(txtE);
+
+        this.getChildren().add(border);
+
+        if (value <= 100 && value >= 0)
+        {
+            if (value >= 0 && value <= 10)
+            {
+                this.getChildren().addAll(blue);
+            }
+
+            if (value >= 11 && value <= 20)
+            {
+                this.getChildren().addAll(blue, bluegreen);
+            }
+
+            if (value >= 21 && value <= 30)
+            {
+                this.getChildren().addAll(blue, bluegreen, green);
+            }
+
+            if (value >= 31 && value <= 40)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow);
+            }
+
+            if (value >= 41 && value <= 50)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow);
+            }
+
+            if (value >= 51 && value <= 60)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow, yelloworange);
+            }
+
+            if (value >= 61 && value <= 70)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow, yelloworange, orange);
+            }
+
+            if (value >= 71 && value <= 80)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow, yelloworange, orange
+                        , orangered);
+            }
+
+            if (value >= 81 && value <= 90)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow, yelloworange, orange
+                        , orangered, red);
+            }
+
+            if (value >= 91 && value <= 100)
+            {
+                this.getChildren().addAll(blue, bluegreen, green, greenyellow, yellow, yelloworange, orange
+                        , orangered, red, redpurple);
+
+            }
+        }
     }
 
     public void init(int width, int height)
